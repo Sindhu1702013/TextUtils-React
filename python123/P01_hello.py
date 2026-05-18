769a4ff2d3f9b1ff50796845aa9dd284a2234421
Sindhu17020103 <sindhuja.golagani@techolution.com>
2026-05-18 17:50:22 +0530

    Added changes

--- python123/P01_hello.py
+++ python123/P01_hello.py
@@ -1,9 +1,9 @@
 # Author: OMKAR PATHAK
 # This program prints the entered message
 
-def justPrint(text):
+def print_text(text):
     '''This function prints the text passed as argument to this function'''
     print(text)
 
 if __name__ == '__main__':
-    justPrint('Hello01 appmod')
+    print_text('Hello01 appmod')
